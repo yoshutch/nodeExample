d96a91a6cd0da323b77e608fc3c40ae917d58377
Scott Hutchings <yoshutch@gmail.com>
2016-01-04 13:01:07 -0700

    booyah

--- my_web_server.js
+++ my_web_server.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end("BOOYAH I Rule!");
 }).listen(8080);
 
 console.log("Server running on port 8080");
